06b7f16b0cf79a6d4bc3d74a015d6cb6bc09e3ec
syncm23 <karolis.liutac@gmail.com>
2017-11-22 23:32:38 +0200

    new

--- xxxxx.php
+++ xxxxx.php
@@ -1,10 +1,17 @@
+<!DOCTYPE html>
 
+
+
+<body>
+<h1> KIK </h1>
 <?php
 
 $mokiniai = [
 ['vardas' => 'Jonas', 'pazymiai' => ['lietuviu' => [4, 8, 6, 7], 'anglu' =>[6, 7, 8], 'matematika' => [3, 5, 4]]], 
 ['vardas' => 'Ona', 'pazymiai' => ['lietuviu' => [10, 9, 10], 'anglu' => [9, 8, 10], 'matematika' => [10, 10, 9, 9]]]
 ];
+
+
 function vidurkis($pazymiai)
 {
     $sum = 0;
@@ -34,8 +41,52 @@ foreach ($mokiniai as $i => $m)
    $mokiniai[$i]['vidurkis']=vidurkis($mokiniai[$i]['trimestras']);
 }
 
-for($i =0; $i < count($mokiniai); $i++) {
-echo $mokiniai[$i]['vidurkis'].' ';
 
-}
+var_dump($mokiniai);
+
+
 ?>
+
+
+
+
+
+<table border="1">
+<tr>
+<th> Vardas</th>
+<th>Pazymiai</th>
+
+</tr>
+<?php foreach($mokiniai as $mokinys): ?>
+<tr>
+<td><?php echo $mokinys['vardas'];?></td>
+<td>
+<?php foreach ($mokinys['pazymiai'] as $dalykas=> $pazymiai): ?>
+<div>
+<?php echo $dalykas. ': '; 
+
+ foreach ($pazymiai as $pazymys): 
+ echo $pazymys. ' '; 
+
+ endforeach; ?>
+
+
+
+</div>
+
+
+
+<?php endforeach; ?>
+
+
+</td>
+
+
+
+</tr>
+
+<?php endforeach; ?>
+</table>
+
+</body>
+</html>
